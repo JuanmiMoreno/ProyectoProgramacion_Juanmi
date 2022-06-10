ee83539b2bfaf704529e5b768977f4c9a6e41a16
Juanmi Moreno <juanmimoreno9@gmail.com>
2022-06-10 13:26:36 +0200

    Documentacion Apero

--- ProyectoJuanmi/src/main/java/clases/Apero.java
+++ ProyectoJuanmi/src/main/java/clases/Apero.java
@@ -9,10 +9,25 @@ import enums.Provincia;
 import enums.TipoApero;
 import exceptions.AñoInvalidoExceptions;
 import utils.UtilsDB;
-
+/**
+ * Clase que instancia un apero  es la herramienta con la que los tractores trabajan para realizar las actividades
+ * @author Juanmi
+ *
+ */
 public class Apero extends Maquinaria {
+	/** El tipo de apero que puede ser arrastrado o suspendido**/
 	private TipoApero tipo;
-
+	
+	/**
+	 * Constructor de apero que inserta apero en la base de datos
+	 * @param marca recibido por el super hace referencia a la marca del apero
+	 * @param modelo recibido por el super hace referencia al modelo del apero
+	 * @param añoAdquisicion recibido por el super hace referencia al año en el que se compro el apero
+	 * @param tipo enum que puede elegir entre suspendido o arrastrado
+	 * @param empresa recibida por el super hace referencia a la empresa que pertence el apero
+	 * @throws SQLException error de la base de datos
+	 * @throws AñoInvalidoExceptions error creado para que el año de adquisicion no pueda ser inferior a 1920 ni superior a 2022 (año actual)
+	 */
 	public Apero(String marca, String modelo, short añoAdquisicion, TipoApero tipo, Empresa empresa) throws SQLException, AñoInvalidoExceptions {
 		super(marca, modelo, añoAdquisicion, empresa);
 		Statement queryInsertar = UtilsDB.conectarBD();
@@ -31,18 +46,33 @@ public class Apero extends Maquinaria {
 
 	}
 
-	public Apero() {
+	/**
+	 * Constructor privado vacido para poder trabajar con la funcion get todos
+	 */
+	private Apero() {
 		
 	}
 	
+	/**
+	 * Getter del tipo de apero 
+	 * @return devuelve el tipo de apero
+	 */
 	public TipoApero getTipo() {
 		return tipo;
 	}
 
+	/**
+	 * Setter del tipo de apero 
+	 * @param tipo nuevo tiò de apero
+	 */
 	public void setTipo(TipoApero tipo) {
 		this.tipo = tipo;
 	}
 
+	/**
+	 * Funcion que crea un array list de apero en el que se inserta todos los apero de la empresa que estan en la base de datos
+	 * @return devuelve un array list de todos los aperos que pertencen a la empresa
+	 */
 	public  static ArrayList<Apero> getTodos()  {
 		Statement smt = UtilsDB.conectarBD();
 		ArrayList<Apero> ret = new ArrayList<Apero>();
@@ -66,6 +96,11 @@ public class Apero extends Maquinaria {
 		return ret;
 	}
 	
+	
+	/**
+	 * Funcion que elimina de la base de datos y pone todo sus variables a null segun el marca del apero
+	 * @return el apero eliminado de la base de datos y en java todas sus variables a null
+	 */
 	public boolean eliminar() {
 
 		Statement smt = UtilsDB.conectarBD();
